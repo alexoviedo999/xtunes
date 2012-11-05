f056356bcb3d21b6dd671e8f1f8f2c85e0b4ec17
alexoviedo999 <alexoviedo999@gmail.com>
2012-11-05 12:02:49 -0500

    Upcases AWS constant

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => ENV['aws_access_key'], # required
-    :aws_secret_access_key  => ENV['aws_secret_key'] # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'], # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] # required
   }
   config.fog_directory  = 'xtunes-alex'                     # required
  # config.fog_host       = 'https://s3.amazonaws.com'            # optional, defaults to nil
