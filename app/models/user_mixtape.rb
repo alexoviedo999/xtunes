fb9826d575e5aca86d537d02fee09d989c805fd0
alexoviedo999 <alexoviedo999@gmail.com>
2012-11-04 21:43:34 -0500

    Migrated new mixtape category related dbs and models

---
+++ app/models/user_mixtape.rb
@@ -0,0 +1,19 @@
+class UserMixtape < ActiveRecord::Base
+  attr_accessible :mixtape_id, :permission, :user_id, :user
+
+  Permissions = {:own => 0, :edit => 10, :listen => 100}
+
+  Permissions.each do |action, level|
+    define_method "#{action}able?" do
+      true if self.permission <= Permissions[action.to_sym]
+    end
+  end
+
+  belongs_to :user
+  belongs_to :mixtape
+
+  def set_permission(permission)
+    self.permission = Permissions[permission]
+    self.save
+  end
+end
