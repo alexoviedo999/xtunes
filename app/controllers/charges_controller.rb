02f11578925cafd07005814d6bdd1364c539aba7
alexoviedo999 <alexoviedo999@gmail.com>
2012-11-08 19:24:25 -0500

    Debugged Stripe functionality by converting purchase price to cents and converting to currency.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,22 +1,24 @@
 class ChargesController < ApplicationController
 
   def index
+    @purchase_price_in_cents = (session[:purchase_amount] * 100).to_i
     render :new
   end
+
   def new
   end
 
   def create
     # Amount in cents
-    @amount = @purchase.price
+    @purchase_price_in_cents = (session[:purchase_amount] * 100).to_i
 
     Stripe::Charge.create(
-      :amount      => (@amount*100),
+      :amount      => @purchase_price_in_cents,
       :card        => params[:stripeToken],
       :description => "test@test.com",
       :currency    => 'usd'
     )
-
+  redirect_to purchases_url
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to charges_path
