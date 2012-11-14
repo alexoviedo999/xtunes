900466f45ba3e695ca30807579e1933e5b22399a
alexoviedo999 <alexoviedo999@gmail.com>
2012-11-14 15:10:57 -0500

    Enabled production assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Xtunes::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.enabled = true
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
